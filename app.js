ac72627fc34b2f3308310ccfb422b1a1dbe140f5
Sourabh <sourabh4pvt@gmail.com>
2024-04-25 10:57:32 +0530

    added cors url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "https://nodejs-todo-3062.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
